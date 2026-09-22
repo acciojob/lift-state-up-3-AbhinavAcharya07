6929c083aea038483d4a8f363691b5dc63d72e7d
Abhinav Acharya <abhinavacharya707@gmail.com>
2026-09-22 17:03:11 +0530

    Add child components to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import ChildComponent1 from './ChildComponent1';
+import ChildComponent2 from './ChildComponent2';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState('');
 
-export default App
+  return (
+    <div className="parent" style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h2>Parent Component</h2>
+      <p>Current Selected Option: <strong>{selectedOption || 'None'}</strong></p>
+      <ChildComponent1 onSelect={setSelectedOption} />
+      <ChildComponent2 onSelect={setSelectedOption} />
+    </div>
+  );
+};
+
+export default App;
